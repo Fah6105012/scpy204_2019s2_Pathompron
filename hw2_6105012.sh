c964f2422a7909042d07918c0f7736363fc367b6
Fah6105012 <64856988+Fah6105012@users.noreply.github.com>
2020-05-08 20:53:39 +0700

    Update hw2_6105012.sh

--- hw2_6105012.sh
+++ hw2_6105012.sh
@@ -12,14 +12,14 @@ do
 	while read line
 	do
 		        for word in $line
-				        do
-						        if [ $word == "Thailand" ]
-								        then
-										        echo -e "$n: $line"
-											        n=$((n+1))
-												        fi
-													        done
-													done < $File
-													gawk -F, '$7 == "Thailand" {s+=$5}END{print "cases in Thailand=",s}' covid_19.txt
-													gawk -F, '$7 == "Thailand" {s+=$5}END{print "cases/population=",s*100/69428524"%"}' covid_19.txt
-													gawk -F, '$7 == "Thailand" {r+=$5; t+=$6} END{print "deaths/cases=",t*100/r"%"}' covid_19.txt
+			do
+					if [ $word == "Thailand" ]
+				        then
+				        echo -e "$n: $line"
+				        n=$((n+1))
+				        fi
+			done
+	done < $File
+gawk -F, '$7 == "Thailand" {s+=$5}END{print "cases in Thailand=",s}' covid_19.txt
+gawk -F, '$7 == "Thailand" {s+=$5}END{print "cases/population=",s*100/69428524"%"}' covid_19.txt
+gawk -F, '$7 == "Thailand" {r+=$5; t+=$6} END{print "deaths/cases=",t*100/r"%"}' covid_19.txt
